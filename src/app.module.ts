d837df5ec5f460e8a3993af62b4848f9284ef86d
IhsanSonz <mnurihsanmutaqin66@gmail.com>
2025-04-16 00:01:36 +0700

    feat: delete old user module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { DatabaseModule } from './database/database.module';
-import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 
 @Module({
@@ -10,7 +9,6 @@ import { AuthModule } from './auth/auth.module';
       isGlobal: true,
     }),
     DatabaseModule,
-    UsersModule,
     AuthModule,
   ],
 })
